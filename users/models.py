b330e3d7848a33ec6c06bf0747c5ed302f4fdc3e
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-11-23 14:27:22 +0900

    테스트

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    age  = models.IntegerField()
+    job  = models.CharField(max_length=100)
+
+    class Meta: 
+        db_table = "persons"
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'menus'
