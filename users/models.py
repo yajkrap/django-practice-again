ebf2c900501b2827bffc43b58122d6f2b2d2d852
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-11-23 15:53:17 +0900

    Crud1-1

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from typing import Sized
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    age  = models.IntegerField()
+    job  = models.CharField(max_length=100)
+
+    class Meta: 
+        db_table = "persons"
